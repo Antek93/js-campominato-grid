ad2b0e5737ba060fdddf4a5ef263e371983ec008
Antek93 <antonio.savino1993@gmail.com>
2023-01-11 13:16:26 +0100

    aggiunto readme

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('HI, I am java and I am working fine');
+
+// A. L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
+// B. Ogni cella ha un numero progressivo, da 1 a 100.
+
+// C. Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+
+// D. Quando l'utente clicca su ogni cella, la cella cliccata
+//    si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
